63a2db0a25662ff0644b5bf9b9da2c898e5b78c4
Aaron <aarongillies88@gmail.com>
2020-02-01 18:40:38 -0500

    Added support for postgresQL and rebuilt knexfile
    Co-authored-by: ethyl2 <zzzuzz@gmail.com>

--- knexfile.js
+++ knexfile.js
@@ -21,41 +21,24 @@ module.exports = {
     }
   },
 
-  // staging: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './data/users.db3'
-  //   },
-  //   useNullAsDefault: true,
-  //   migrations: {
-  //     directory: './data/migrations'
-  //   },
-  //   seeds: {
-  //     directory: './data/seeds'
-  //   },
-  //   pool: {
-  //     afterCreate: (conn, done) => {
-  //       conn.run('PRAGMA foreign_keys = ON', done);
-  //     }
-  //   }
-  // },
-
-  // production: {
-  //   client: 'pg',
-  //   connection: {
-  //     filename: './data/users.db3'
-  //   },
-  //   useNullAsDefault: true,
-  //   migrations: {
-  //     directory: './data/migrations'
-  //   },
-  //   seeds: {
-  //     directory: './data/seeds'
-  //   },
-  //   pool: {
-  //     afterCreate: (conn, done) => {
-  //       conn.run('PRAGMA foreign_keys = ON', done);
-  //     }
-  //   }
-  // },
+  production: {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      post: 5432,
+      user: 'postgres',
+      password: 'Lambda1991',
+      database: 'users'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    }
+  },
 };
