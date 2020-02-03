e9293bd5fa456aea2dd60e5a891a56c37f4517b3
Aaron <aarongillies88@gmail.com>
2020-02-03 17:12:37 -0500

    Added testing for register/login
    Co-authored-by: ethyl2 <zzzuzz@gmail.com>

---
+++ users/users.test.js
@@ -0,0 +1,76 @@
+const request = require('supertest');
+const server = require('../api/server');
+
+describe('user router', () => {
+    it('should register a user and give a status code back of 201', () => {
+        let randomNum = Math.random() * 8000;
+        return request(server)
+            .post('/api/register')
+            .send({
+                "username": `tester${randomNum}`,
+                "password": "LambdaTesting",
+                "name": "Tester"
+            })
+            .then(res => {
+                expect(res.status).toBe(201);
+            });
+    });
+    it('Should get token back', () => {
+        let randomNum = Math.random() * 8000;
+        return request(server)
+        .post('/api/register')
+        .send({
+            "username": `tester${randomNum}`,
+            "password": "LambdaTesting",
+            "name": "Tester"
+        })
+        .then(res => {
+            expect(res.body.token).toBeDefined()
+        })
+    })
+});
+
+describe('User Router', () => {
+    it('Successful login results in status 200', () => {
+        let randomNum = Math.random() * 8000;
+        return request(server)
+        .post('/api/register')
+        .send({
+            "username": `tester${randomNum}`,
+            "password": "LambdaTesting",
+            "name": "Tester" 
+        })
+        .then(res => {
+            return request(server)
+            .post('/api/login')
+            .send({
+                "username": `tester${randomNum}`, 
+                "password": "LambdaTesting"
+            })
+            .then(response => {
+                expect(response.status).toBe(200)
+            })
+        })
+    })
+    // it('Successful login returns a token', () => {
+    //     let randomNum = Math.random() * 8000;
+    //     return request(server)
+    //     .post('/api/register')
+    //     .send({
+    //         "username": `tester${randomNum}`,
+    //         "password": "LambdaTesting",
+    //         "name": "Tester" 
+    //     })
+    //     .then(res => {
+    //         return request(server)
+    //         .post('/api/login')
+    //         .send({
+    //             "username": `tester${randomNum}`, 
+    //             "password": "LambdaTesting"
+    //         })
+    //         .then(response => {
+    //             expect(response.status).toBe(200)
+    //         })
+    //     })
+})
+
